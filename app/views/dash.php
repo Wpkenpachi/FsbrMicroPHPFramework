a4acc87d63d6496332b812e44ddb4a56436adf95
Wesley Paulo <wesley_paulo_job@outlook.com>
2017-06-26 12:40:44 -0300

    Ignoring testes folder and Deleting AdminLTE

--- app/views/dash.php
+++
@@ -1,9 +0,0 @@
-<div>
-    <h1> Bem vindo ao seu Dashboard {{ user['nome'] }}</h1>
-    <p>
-        Suas informações pessoais:
-        <li> Nome: {{ user['nome'] }} </li>
-        <li >Email: {{ user['email'] }} </ti>
-
-    </p>
-</div>
